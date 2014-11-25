e26780b49babe1fc1505c46cfea14f47cc86f1af
Ben Ibinson <codehex.net@gmail.com>
2014-11-25 19:45:48 +0000

    More commenting

--- src/Bugsnag.Core/Payload/Event/StackTraceFrameInfo.cs
+++ src/Bugsnag.Core/Payload/Event/StackTraceFrameInfo.cs
@@ -2,20 +2,39 @@
 
 namespace Bugsnag.Core.Payload
 {
+    /// <summary>
+    /// Contains information about a single frame of a stack trace
+    /// </summary>
     public class StackTraceFrameInfo
     {
+        /// <summary>
+        /// Gets or sets the filename of the file the frame occurred (optional)
+        /// </summary>
         [JsonProperty("file")]
         public string File { get; set; }
 
+        /// <summary>
+        /// Gets or sets the line number of the frame
+        /// </summary>
         [JsonProperty("lineNumber")]
         public int? LineNumber { get; set; }
 
+        /// <summary>
+        /// Gets or sets the column number of the frame (optional)
+        /// </summary>
         [JsonProperty("columnNumber")]
         public int? ColumnNumber { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the method this stack frame is in
+        /// </summary>
         [JsonProperty("method")]
         public string Method { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the frame occurred in the users code. Allows filtering to only show frames
+        /// that occur in the users code (optional, defaults to false)
+        /// </summary>
         [JsonProperty("inProject")]
         public bool InProject { get; set; }
     }
--- src/Bugsnag.Core/Payload/Notification.cs
+++ src/Bugsnag.Core/Payload/Notification.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Bugsnag.Core.Payload
 {
+    /// <summary>
+    /// Represents a single notification object to send to Bugsnag
+    /// </summary>
     public class Notification
     {
+        /// <summary>
+        /// Gets or sets the API key associated with the Bugsnag account to send the notification to
+        /// </summary>
         [JsonProperty("apiKey")]
         public string ApiKey { get; set; }
 
+        /// <summary>
+        /// Gets or sets the information about the notifier used 
+        /// </summary>
         [JsonProperty("notifier")]
         public NotifierInfo Notifier { get; set; }
 
+        /// <summary>
+        /// Gets or sets the list of events to send in the notification (normally one event)
+        /// </summary>
         [JsonProperty("events")]
         public List<EventInfo> Events { get; set; }
     }
