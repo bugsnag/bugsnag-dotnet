054df4d5d3e01b415fe5164619cbcd5c43c6909d
Ben Ibinson <codehex.net@gmail.com>
2015-03-22 19:57:33 +0000

    Updated build script for multiple builds

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,13 @@
 properties {
   # Custom properties
-  $default_version = "1.1.0.0"
-  $config = "Debug"
+  $default_version = "1.3.0.0"
+  
+  # Build profiles
+  $profiles = @(
+    @{config = "Debug"; test_only=$true},
+    @{config = "Release"; nuspec="Bugsnag.nuspec"},
+    @{config = "MonoRelease"; nuspec="BugsnagMono.nuspec"}
+  )
   
   # Bugsnag Projects
   $projects = @(
@@ -14,14 +20,11 @@ properties {
   $output_dir = "$base_dir\output"
   $build_dir = "$output_dir\bin"
   $test_dir = "$output_dir\test"
-  
-  # Nuget Directories
   $nuget_dir = "$output_dir\nuget"
-  $nuget_lib_dir = "$nuget_dir\lib"
-  $nuget_spec_template = "$base_dir\Bugsnag.nuspec"
-  $nuget_spec = "$nuget_dir\Bugsnag.nuspec"
   
   # Misc directorires
+  $src_files_dir = "$base_dir\..\src"
+  $test_files_dir = "$base_dir\..\test"
   $tools_dir = "$base_dir\..\tools"
   $packages_dir = "$base_dir\..\packages"
   $sln_file = "$base_dir\..\Bugsnag.sln"
@@ -31,59 +34,74 @@ properties {
 task default -depends Package, Archive
 
 task Package -depends Test {
-	mkdir $nuget_dir | Out-Null
-	
-	log "Updating Nuspec file (Version:$version)" 
-	Copy-Item $nuget_spec_template $nuget_spec
-	$spec = [xml](Get-Content $nuget_spec )
-    $spec.package.metadata.version = $version
-    $spec.Save($nuget_spec)
+  foreach($profile in ($profiles | Where-Object {$_.nuspec})) {
+    $nuget_profile_dir = "$nuget_dir\$($profile.config)"
+    mkdir $nuget_profile_dir | Out-Null
+    
+    log "Updating Nuspec file for $($profile.config) (Version:$version)"
+	$template_spec = "$base_dir\$($profile.nuspec)"
+    $new_spec = "$nuget_profile_dir\$($profile.nuspec)"	
+    Copy-Item $template_spec $new_spec
+    $spec_xml = [xml](Get-Content $new_spec)
+    $spec_xml.package.metadata.version = $version
+    $spec_xml.Save($new_spec)
 
-	log "Creating Nuget package" 
-    mkdir $nuget_lib_dir | Out-Null
-    Copy-Item "$build_dir\*" $nuget_lib_dir -Recurse
-	Remove-Item "$nuget_lib_dir\*" -Exclude "Bugsnag.*" -Recurse
-    exec { nuget pack $nuget_spec -OutputDirectory $output_dir }
+    log "Creating Nuget package for $($profile.config)"
+    $new_lib_dir = "$nuget_profile_dir\lib"
+    mkdir $new_lib_dir | Out-Null
+    Copy-Item "$build_dir\$($profile.config)\*" $new_lib_dir -Recurse
+    Remove-Item "$new_lib_dir\*" -Exclude "Bugsnag*" -Recurse
+    exec { nuget pack $new_spec -OutputDirectory $output_dir -Verbosity quiet}
+  }
 }
 
 task Archive -depends Test {
   if ($env:APPVEYOR) {
-	log "Skipping archiving, Appveyor will publish archive as artifact"
+    log "Skipping archiving, Appveyor will publish archive as artifact"
   } else {
     log "Creating zip file of binaries"
     mkdir "$output_dir\archive" | Out-Null   
     Copy-Item $build_dir "$output_dir\archive" -Recurse
     Write-Zip -Path "$output_dir\archive\*" -OutputPath "$output_dir\bugsnag.$version.zip" | Out-Null  
-	Remove-Item "$output_dir\archive" -Recurse
+    Remove-Item "$output_dir\archive" -Recurse
   }
 }
 
 task Test -depends Compile, Clean {
   foreach($project in $projects) {
-	log "Running unit tests for $($project.name)"
-	$test_name = "$($project.name).Test"
-	$test_project_file = "..\test\$test_name\$test_name.csproj"
-	$test_output = "$test_dir\$($project.target)"
-	
-	compile_project $test_project_file $test_output
-	if ($env:APPVEYOR) {
-	  exec {& xunit.console.clr4 $("$test_output\$test_name.dll") /noshadow /appveyor}
-    } else {	  
-	  exec {& $tools_dir/xunit/xunit.console.clr4.exe $("$test_output\$test_name.dll") /noshadow }
+    foreach($profile in $profiles) {
+      log "Running unit tests for $($project.name) ($($profile.config))"
+      $test_name = "$($project.name).Test"
+      $test_project_file = "$test_files_dir\$test_name\$test_name.csproj"
+      $test_output = "$test_dir\$($profile.config)\$($project.name)"
+    
+      compile_project $test_project_file $profile.config $test_output 
+      if ($env:APPVEYOR) {
+        exec {& xunit.console.clr4 $("$test_output\$test_name.dll") /noshadow /appveyor}
+      } else {      
+        exec {& $tools_dir/xunit/xunit.console.clr4.exe $("$test_output\$test_name.dll") /noshadow }
+      }
 	}
   }
 }
 
 task Compile -depends Clean {
   log "Patching assembly versions to $version"
-  replace_line "..\src\Common\CommonVersionInfo.cs" "^\[assembly: AssemblyVersion.*$" "[assembly: AssemblyVersion(`"$version`")]"
-  replace_line "..\src\Common\CommonVersionInfo.cs" "^\[assembly: AssemblyFileVersion.*$" "[assembly: AssemblyFileVersion(`"$version`")]"
+  replace_line "$src_files_dir\Common\CommonVersionInfo.cs" "^\[assembly: AssemblyVersion.*$" "[assembly: AssemblyVersion(`"$version`")]"
+  replace_line "$src_files_dir\Common\CommonVersionInfo.cs" "^\[assembly: AssemblyFileVersion.*$" "[assembly: AssemblyFileVersion(`"$version`")]"
   
   foreach($project in $projects) {
-    log "Building $($project.name)"
-	$project_file = "..\src\$($project.name)\$($project.name).csproj"
-	$project_out = "$build_dir\$($project.target)\"
-	compile_project $project_file $project_out
+    foreach($profile in $profiles) {
+      if(!$profile.test_only) {
+        log "Building $($project.name) ($($profile.config))"
+        $project_file = "$src_files_dir\$($project.name)\$($project.name).csproj"
+        $project_out = "$build_dir\$($profile.config)"
+        if ($project.target) {
+          $project_out = "$project_out\$($project.target)\"
+        }
+        compile_project $project_file $profile.config $project_out
+      }
+    }
   }
 }
 
@@ -100,7 +118,7 @@ function global:log($log_text)
   Write-Host $log_text -foregroundcolor "Yellow"
 }
 
-function global:compile_project($project_file, $out_dir)
+function global:compile_project($project_file, $config, $out_dir)
 {
   $package_file = $(Split-Path $project_file) + $("\packages.config")
   nuget restore $package_file -PackagesDirectory $packages_dir | Out-Null
@@ -112,4 +130,3 @@ function global:replace_line($file, $regex, $newline)
   $content = Get-Content $file
   $content -replace $regex, $newline | Set-Content $file
 }
-
