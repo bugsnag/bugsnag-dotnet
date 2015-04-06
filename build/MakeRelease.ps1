adf2d28631aef2fd2c52fe377a583999c997b3a4
Paul Betts <paul@paulbetts.org>
2015-04-05 17:55:21 -0700

    Tests are p. cool

--- build/MakeRelease.ps1
+++ build/MakeRelease.ps1
@@ -1,3 +1,21 @@
 $baseDir = Resolve-Path "$PSScriptRoot/.."
+$msbuildExe = "C:\Program Files (x86)\MSBuild\12.0\bin\MsBuild.exe"
+$nugetExe = "$baseDir\.nuget\NuGet.exe"
+
+$configsToBuild = {"Release", "MonoRelease"}
+
+if ($env:APPVEYOR) {
+  $testExe = "xunit.console.clr4"
+  $testFlags = '/appveyor'
+} else {
+  $testExe = "$baseDir\tools\xunit\xunit.console.clr4.exe"
+  $testFlags = ''
+}
 
 git clean -xdf
+& "$nugetExe" restore "$baseDir\Bugsnag.sln"
+
+foreach-object $configsToBuild | % { & $msbuildExe "$baseDir/Bugsnag.sln" "/p:Configuration=$_" }
+
+$filesToTest = ls -r "$baseDir\test\bin\**\Bugsnag.Test.dll"
+echo $filesToTest | % { & $testExe "$_" $testFlags }
