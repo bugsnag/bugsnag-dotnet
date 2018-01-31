0e641b0fcf12df2c170f6afcbf7dd895ef63f6aa
Martin Holman <martin@bugsnag.com>
2018-01-31 10:15:40 -0800

    Smaller number of tests for speed

--- tests/Bugsnag.Tests/TransportTests.cs
+++ tests/Bugsnag.Tests/TransportTests.cs
@@ -11,12 +11,13 @@ namespace Bugsnag.Tests
     [Fact]
     public async void Test()
     {
-      var server = new TestServer(5001, 20);
+      var numerOfRequests = 1;
+      var server = new TestServer(5001, numerOfRequests);
       server.Start();
 
       var endpoint = new Uri("http://localhost:5001");
       var transport = new Transport();
-      for (int i = 0; i < 20; i++)
+      for (int i = 0; i < numerOfRequests; i++)
       {
         var rawPayload = System.Text.Encoding.UTF8.GetBytes($"{{ \"count\": {i} }}");
         var responseCode = await Task.Factory.FromAsync((callback, state) => transport.BeginSend(endpoint, rawPayload, callback, state), transport.EndSend, null);
@@ -25,7 +26,7 @@ namespace Bugsnag.Tests
 
       var requests = await server.Requests();
 
-      Assert.Equal(20, requests.Count());
+      Assert.Equal(numerOfRequests, requests.Count());
     }
   }
 }
