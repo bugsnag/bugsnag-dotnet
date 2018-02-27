a45a4427f575a7cf8c66d8b9b442ded2aab70f8c
Martin Holman <martin@bugsnag.com>
2018-02-26 18:30:32 -0800

    Test that a non thrown exception produces a report

--- tests/Bugsnag.Tests/ReportTests.cs
+++ tests/Bugsnag.Tests/ReportTests.cs
@@ -23,5 +23,15 @@ namespace Bugsnag.Tests
       var report = new Report(configuration, exception, Bugsnag.Payload.Severity.ForHandledException(), new Breadcrumb[] { new Breadcrumb("test", BreadcrumbType.Manual) }, new Session(), new Request());
       Assert.NotNull(report);
     }
+
+    [Fact]
+    public void NonThrownException()
+    {
+      System.Exception exception = new System.Exception("test");
+      var configuration = new Configuration("123456");
+
+      var report = new Report(configuration, exception, Bugsnag.Payload.Severity.ForHandledException(), new Breadcrumb[] { new Breadcrumb("test", BreadcrumbType.Manual) }, new Session(), new Request());
+      Assert.NotNull(report);
+    }
   }
 }
