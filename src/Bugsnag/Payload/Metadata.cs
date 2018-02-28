0057fc7bed11333bc2d04d4e2f84c31eed5ede39
Martin Holman <martin@bugsnag.com>
2018-02-28 11:24:10 -0800

    nail_care: spelling

--- src/Bugsnag/Payload/Metadata.cs
+++ src/Bugsnag/Payload/Metadata.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace Bugsnag.Payload
 {
   /// <summary>
-  /// Represnts Metadata that can be attached to error reports sent to Bugsnag.
+  /// Represents Metadata that can be attached to error reports sent to Bugsnag.
   /// </summary>
   public class Metadata : Dictionary<string, object>
   {
