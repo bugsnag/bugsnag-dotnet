de11849d09d2b83e9d4b3ed695649f5d365f19c1
Martin Holman <martin@bugsnag.com>
2018-03-13 18:13:49 -0700

    Handle aggregate exceptions

--- src/Bugsnag/Payload/Exception.cs
+++ src/Bugsnag/Payload/Exception.cs
@@ -34,27 +34,39 @@ namespace Bugsnag.Payload
 
     private static IEnumerable<System.Exception> FlattenAndReverseExceptionTree(System.Exception ex)
     {
-      // ReflectionTypeLoadException is special because the details are in
-      // the LoaderExceptions property
-      if (ex is ReflectionTypeLoadException typeLoadException)
-      {
-        var typeLoadExceptions = new List<System.Exception>();
-        foreach (var loadException in typeLoadException.LoaderExceptions)
-        {
-          typeLoadExceptions.AddRange(FlattenAndReverseExceptionTree(loadException));
-        }
+      if (ex == null) yield break;
 
-        typeLoadExceptions.Add(ex);
-        return typeLoadExceptions;
+      switch (ex)
+      {
+        case ReflectionTypeLoadException typeLoadException:
+          foreach (var exception in typeLoadException.LoaderExceptions)
+          {
+            foreach (var item in FlattenAndReverseExceptionTree(exception))
+            {
+              yield return item;
+            }
+          }
+          break;
+#if !NET35
+        case System.AggregateException aggregateException:
+          foreach (var exception in aggregateException.InnerExceptions)
+          {
+            foreach (var item in FlattenAndReverseExceptionTree(exception))
+            {
+              yield return item;
+            }
+          }
+          break;
+#endif
+        default:
+          foreach (var item in FlattenAndReverseExceptionTree(ex.InnerException))
+          {
+            yield return item;
+          }
+          break;
       }
 
-      var list = new List<System.Exception>();
-      while (ex != null)
-      {
-        list.Add(ex);
-        ex = ex.InnerException;
-      }
-      return list;
+      yield return ex;
     }
   }
 
