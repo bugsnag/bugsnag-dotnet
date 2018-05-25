907b9f7cff7597fb983aaf553aa059e10d70f40a
Martin Holman <martin@bugsnag.com>
2018-05-25 13:44:02 -0700

    Skip sending session data when there is none
    Check that we have session data to send before we send the payload to Bugsnag

--- tests/Bugsnag.Tests/SessionTrackingTests.cs
+++ tests/Bugsnag.Tests/SessionTrackingTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Xunit;
 
 namespace Bugsnag.Tests
@@ -20,13 +21,40 @@ namespace Bugsnag.Tests
 
       server.Start();
 
-      var sessionTracking = new SessionTracker(new Configuration("123456") { SessionTrackingInterval = TimeSpan.FromSeconds(5), SessionEndpoint = server.Endpoint });
+      var sessionTracking = new SessionTracker(new Configuration("123456") {
+        SessionEndpoint = server.Endpoint
+      });
 
       sessionTracking.CreateSession();
 
       var requests = await server.Requests(1);
 
       Assert.NotNull(sessionTracking.CurrentSession);
+    }
+
+    [Fact]
+    public async void CurrentSessionCanBeSetO()
+    {
+      var server = new TestServer();
+
+      server.Start();
+
+      var sessionTracking = new SessionTracker(new Configuration("123456") {
+        SessionEndpoint = server.Endpoint
+      });
+
+      sessionTracking.CreateSession();
+
+      // set the cancellation token length long enough so that the session
+      // tracker would attempt to send sessions twice
+      var cts = new CancellationTokenSource(TimeSpan.FromSeconds(130));
+
+      // the cancellation token should throw as we are 'expecting' 2 requests
+      // to be received but we only want one to be sent as the second sessions
+      // request will have no sessions data
+      await Assert.ThrowsAsync<OperationCanceledException>(async () => {
+        await server.Requests(2, cts.Token);
+      });
     }
   }
 }
