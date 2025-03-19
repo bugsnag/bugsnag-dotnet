56b0b002af523fcff3bbc6bb58ef685310456df5
Ahmed Ahmed <yousif@bugsnag.com>
2025-03-19 15:54:41 +0000

    remove net45 example

--- examples/aspnet45-mvc-webapi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace aspnet45_mvc_webapi.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
